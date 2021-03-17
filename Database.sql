2157db79ad9f085e3956042245a73683549e38bf
Noah Albers <albers-noah@pm.me>
2021-03-17 17:18:09 +0100

    Added permissions for admins using a binary style. Eg. 0b010

--- Database.sql
+++ Database.sql
@@ -3,7 +3,7 @@ CREATE TABLE `user` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `firstname` VARCHAR(30) NOT NULL,
   `lastname` VARCHAR(30) NOT NULL,
-  `plz` INT(5) NOT NULL,
+  `postalcode` INT(5) NOT NULL,
   `location` VARCHAR(100) NOT NULL,
   `street` VARCHAR(50) NOT NULL,
   `housenumber` VARCHAR(10) NOT NULL,
@@ -19,4 +19,22 @@ ALTER TABLE `user`
   ADD UNIQUE KEY `uq_name` (`firstname`,`lastname`);
 
 -- Creates the time spent table
-CREATE TABLE `timespent` ( `id` INT NOT NULL AUTO_INCREMENT , `start` DATETIME NOT NULL , `stop` DATETIME NULL DEFAULT NULL , `enddisconnect` BOOLEAN NOT NULL , `userid` INT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+CREATE TABLE `timespent` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `start` DATETIME NOT NULL ,
+  `stop` DATETIME NULL DEFAULT NULL ,
+  `enddisconnect` BOOLEAN NOT NULL ,
+  `userid` INT NOT NULL ,
+  PRIMARY KEY (`id`)) ENGINE = InnoDB;
+
+-- Creates the admin table
+CREATE TABLE `planmeldung`.`admin` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `name` TEXT NOT NULL ,
+  `authcode` SMALLINT(4) ,
+  `authcodetimeout` DATETIME NOT NULL ,
+  `isfrozen` BOOLEAN NOT NULL ,
+  `telegramchatid` INT NOT NULL ,
+  `clientrsapublic` JSON NOT NULL ,
+  `permissions` INT NOT NULL,
+  PRIMARY KEY (`id`)) ENGINE = InnoDB;
